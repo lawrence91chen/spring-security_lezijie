5887275722ed9d1257d5487d35b097325e52a4a2
lawrence_chen <lawrence91chen@gmail.com>
2022-02-21 14:38:30 +0800

    feat: oauth2 - token store by redis

--- spring-security-oauth2/src/main/java/com/example/config/AuthorizationServerConfig.java
+++ spring-security-oauth2/src/main/java/com/example/config/AuthorizationServerConfig.java
@@ -1,17 +1,34 @@
 package com.example.config;
 
+import com.example.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 	@Autowired
 	private PasswordEncoder passwordEncoder;
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private TokenStore tokenStore;
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(authenticationManager)
+				.userDetailsService(userService)
+				.tokenStore(tokenStore);
+	}
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -29,7 +46,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 				/**
 				 * 授權類型
 				 * authorization_code: 授權碼模式
+				 * password: 密碼模式
 				 */
-				.authorizedGrantTypes("authorization_code");
+				.authorizedGrantTypes("authorization_code", "password");
 	}
 }
